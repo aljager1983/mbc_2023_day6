edab4c47815e8ca21ae466a9ad47b706f8c3c5a1
aldrin <aldrin@DESKTOP-010K1CJ>
2023-05-22 20:15:03 +0800

    finalized frontend and backend

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -94,7 +94,7 @@ document.getElementById("div").addEventListener("click", async function() {
     screen.innerHTML = "Indivisble by 0";
     await backend.reset();
   } else {
-    screen.innerHTML = Math.round(result * 10000000000) / 10000000000;
+    screen.innerHTML = Math.round(result * 100000) / 100000;
   }
   i = [];
   console.log(result);
@@ -134,3 +134,50 @@ document.getElementById("floor").addEventListener("click", async function() {
 
 
 // prepareLoginBotton(render);
+
+
+// const clearConsole = () => {
+//     console.clear();
+// };
+
+// const clearConsoleTime = setTimeout(clearConsole, 2000);
+
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Setting up the letters
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+// Setting up the drops
+var drops = [];
+for (var j = 0; j < columns; j++) {
+  drops[j] = 1;
+}
+
+// Setting up the draw function
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var j = 0; j < drops.length; j++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#0f0';
+    ctx.fillText(text, j * fontSize, drops[j] * fontSize);
+    drops[j]++;
+    if (drops[j] * fontSize > canvas.height && Math.random() > .95) {
+      drops[j] = 0;
+    }
+  }
+}
+
+// Loop the animation
+setInterval(draw, 33);
